77b2fa0c28befd31da0f92d6db912c530bb86ded
Tomas Strand <tomas@fik1.net>
2021-11-21 20:51:29 +0200

    Add day0 as base

---
+++ src/day0.rs
@@ -0,0 +1,28 @@
+use std::io;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn is_even_number() {
+        assert_eq!(false, even_number(1));
+        assert_eq!(true, even_number(2));
+        assert_eq!(false, even_number(3));
+        assert_eq!(true, even_number(4));
+        assert_eq!(false, even_number(5));
+    }
+}
+
+
+pub fn even_number(number: i32) -> bool {
+    number %2 == 0
+}
+
+pub fn run() -> io::Result<()> {
+    println!("\n\nDay 0\n");
+
+    println!("2 is even: {}", even_number(2));
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,12 @@
 use std::io;
 
 // import each day with mod dayX;
-
+mod day0;
 
 fn main() -> io::Result<()> {
     let day = std::env::args().nth(1).expect("No day given");
     match day.parse::<i32>().unwrap() {
+        0 => day0::run()?,
         // Add a match for each day
         // N   =>  dayN::run()?,
 
